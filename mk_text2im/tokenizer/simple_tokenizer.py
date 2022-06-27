e62f54085bb7be10c9098f30578ee91767ade17c
Mohit Kumar <77378821+MohitKumar1239Ultron@users.noreply.github.com>
2022-06-27 07:27:11 +0530

    Update simple_tokenizer.py

--- mk_text2im/tokenizer/simple_tokenizer.py
+++ mk_text2im/tokenizer/simple_tokenizer.py
@@ -1,7 +1,3 @@
-"""
-Copied from: https://github.com/openai/CLIP/blob/573315e83f07b53a61ff5098757e8fc885f1703e/clip/simple_tokenizer.py
-"""
-
 import gzip
 import html
 import os
@@ -19,15 +15,6 @@ def default_bpe():
 
 @lru_cache()
 def bytes_to_unicode():
-    """
-    Returns list of utf-8 byte and a corresponding list of unicode strings.
-    The reversible bpe codes work on unicode strings.
-    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
-    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
-    This is a signficant percentage of your normal, say, 32K bpe vocab.
-    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
-    And avoids mapping to whitespace/control characters the bpe code barfs on.
-    """
     bs = (
         list(range(ord("!"), ord("~") + 1))
         + list(range(ord("¡"), ord("¬") + 1))
@@ -45,9 +32,6 @@ def bytes_to_unicode():
 
 
 def get_pairs(word):
-    """Return set of symbol pairs in a word.
-    Word is represented as tuple of symbols (symbols being variable-length strings).
-    """
     pairs = set()
     prev_char = word[0]
     for char in word[1:]:
